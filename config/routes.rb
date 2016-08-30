4d07a49ccb79d2e384bea1eb05d84cf0fe50d239
yukikaze503 <yukikaze-503@163.com>
2016-08-30 12:41:54 +0000

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
   resources :sessions
 
   get '/signup', to: 'users#new'
-  post ''
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'session#destroy'
+
+  root 'stories#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
