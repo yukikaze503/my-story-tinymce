cad7393cee64c2fa7c93a490386d600b0d13d42d
yukikaze503 <yukikaze-503@163.com>
2016-08-30 12:31:27 +0000

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :stories
+  resources :users
+  resources :sessions
+
+  get '/signup', to: 'users#new'
+  post ''
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
