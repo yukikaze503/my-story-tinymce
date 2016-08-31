9e66d9faa0da90cb9bebbea649b081b4f4dbe8e2
yukikaze503 <yukikaze-503@163.com>
2016-08-31 08:56:38 +0000

    edit css

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -33,11 +33,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [500, 300]
+    process :resize_to_fit => [450, 300]
   end
 
   version :small_thumb, from_version: :thumb do
-    process resize_to_fill: [200, 120]
+    process resize_to_fill: [270, 180]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
